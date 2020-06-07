aeb4aa3e44719a1437b6ec9c5720db6c56bf6943
Ramiro Saenz <saenzramiro@gmail.com>
2020-06-07 00:01:45 -0300

    onDone

--- index.d.ts
+++ index.d.ts
@@ -69,6 +69,13 @@ declare namespace electronDl {
 		@default false
 		*/
 		readonly openFileWhenDone?: boolean;
+		
+		/**
+		Open the downloaded file in the system file manager with the default system app.
+
+		@default false
+		*/
+		readonly onDone?: (path: string) => void;
 
 		/**
 		Shows the file count badge on macOS/Linux dock icons when download is in progress.
